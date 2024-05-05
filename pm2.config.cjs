ae0d0332ca78299880463ccf9215780e343fc058
Tan Jun Yan <yan@yan3321.com>
2024-05-05 14:56:52 +0800

    feat: rename pm2 config to cjs

---
+++ pm2.config.cjs
@@ -0,0 +1,9 @@
+module.exports = {
+    apps: [
+        {
+            name: "Apollo",
+            script: "main.js",
+            cwd: "dist/",
+        },
+    ],
+};
--- pm2.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    apps: [{
-        name: 'Apollo',
-        script: 'main.js',
-        cwd: 'dist/',
-    }],
-}
